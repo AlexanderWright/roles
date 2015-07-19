0d5b6d2efbe3b026ca57627f0a31d94a5f3936d7
Alexander Wright <alexander@silverfish-design.co.uk>
2015-07-19 16:16:05 +0100

    Update RoleHasRelations.php
    Added role validity

--- src/Bican/Roles/Traits/RoleHasRelations.php
+++ src/Bican/Roles/Traits/RoleHasRelations.php
@@ -2,57 +2,29 @@
 
 namespace Bican\Roles\Traits;
 
-trait RoleHasRelations
+trait PermissionHasRelations
 {
     /**
-     * Role belongs to many permissions.
+     * Permission belongs to many roles.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function permissions()
+    public function roles()
     {
-        return $this->belongsToMany(config('roles.models.permission'))->withTimestamps();
+        return $this->belongsToMany(config('roles.models.role'))
+            ->wherePivot('valid_at', '>', Carbon::now())
+            ->wherePivot('expires_at', '<', Carbon::now())
+            ->withPivot('valid_at', 'expires_at')
+            ->withTimestamps();
     }
 
     /**
-     * Role belongs to many users.
+     * Permission belongs to many users.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
     public function users()
     {
         return $this->belongsToMany(config('auth.model'))->withTimestamps();
-    }
-
-    /**
-     * Attach permission to a role.
-     *
-     * @param int|\Bican\Roles\Models\Permission $permission
-     * @return int|bool
-     */
-    public function attachPermission($permission)
-    {
-        return (!$this->permissions()->get()->contains($permission)) ? $this->permissions()->attach($permission) : true;
-    }
-
-    /**
-     * Detach permission from a role.
-     *
-     * @param int|\Bican\Roles\Models\Permission $permission
-     * @return int
-     */
-    public function detachPermission($permission)
-    {
-        return $this->permissions()->detach($permission);
-    }
-
-    /**
-     * Detach all permissions.
-     *
-     * @return int
-     */
-    public function detachAllPermissions()
-    {
-        return $this->permissions()->detach();
     }
 }
